ed3aa4bc144cac63b266c90153a58e063db7d949
The-CoderBoy <gauravjoshi747@gmail.com>
2023-11-06 19:09:50 +0530

    home page create

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
+import style from "@/styles/Home.module.css";
+import { kadwa, grenze } from "@/utils/font";
 
 function index() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <div className="container">
+        <div className={style.logoDiv}>
+          <img
+            src="/image/Govind Hand Print.png"
+            alt="Govind Hand Print"
+            width="200px"
+            height="150px"
+            style={{ marginTop: "15px" }}
+          />
+        </div>
+
+        <div className={style.perentDiv}>
+          <img
+            src="/image/banner900x400.jpg"
+            alt="Govind Hand Print Banner"
+            className={style.img}
+          />
+          <div className={style.card}>
+            <div className={`${kadwa.className} ${style.divHead}`}>
+              <span className={style.p}>Three generation dedicated to</span>
+              <br />
+              <span className={style.headP}>Dabu Print</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={`${grenze.className} ${style.intro}`}>
+          We specialize in the intricate craft of Dabu printing, a traditional
+          art that has been passed down through three generations within our
+          family. Our reputation is built on the foundation of exceptional
+          product quality and a wide range of diverse and intricate designs.
+        </div>
+      </div>
+    </>
   );
 }
 
--- utils/font.tsx
+++ utils/font.tsx
@@ -1,4 +1,4 @@
-import { Kadwa } from "next/font/google";
+import { Kadwa, Grenze } from "next/font/google";
 
 const kadwa = Kadwa({
   weight: ["400", "700"],
@@ -6,4 +6,10 @@ const kadwa = Kadwa({
   subsets: ["latin"],
 });
 
-export { kadwa };
+const grenze = Grenze({
+  weight: ["400", "700", "100", "200", "300", "500", "600", "800", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
+export { kadwa, grenze };
